dfe80071d70fa8864abeec66ceb6073e219fbcec
spidchenko <undead.jesus.christ@gmail.com>
2022-01-11 12:28:30 +0200

    Refactored, replaced old user login calculation

--- src/dbf/swing/DBConnection.java
+++ src/dbf/swing/DBConnection.java
@@ -1,197 +1,196 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dbf.swing;
 
-import java.io.IOException;
+import javax.swing.*;
 import java.sql.*;
-import javax.swing.JOptionPane;
+
 /**
- *
  * @author spidchenko.d
  */
 public class DBConnection {
-    
-    //ВРЕМЕННЫЕ НАСТРОЙКИ, ПАРАМЕТРЫ СОЕДИНЕНИЯ БУДУТ ХРАНИТЬСЯ В ОТДЕЛЬНОМ ФАЙЛЕ
-    
-    //bill ХХ_Х_ХХ_Х_ХХ_ХХХXХ (13 цифр)
+
+    //bill 11_2_33_4_55_66666 (13 цифр)
     //формируется из таких полей:
-    //ХХ-предмет, Х-язык перевода, ХХ-место сдачи, Х-область, ХХ-город/район, ХХХXХ-id пользователя+10000
+    //11-предмет, 2-язык перевода, 33-место сдачи, 4-область, 55-город/район, 66666-login пользователя
 
-    //Если буква "і" отображается в консоли как "?":
-    //набираем в консоли ChCp 1251, в ответ получаем
-    //Текущая кодовая страница: 1251. Теперь запускаем
-    //программу и убеждаемся, что все работает
-
-    
-    //localhost SQL server:
-    //private static final String URL = "jdbc:mysql://localhost:3306/khersontes_db";
-   // private static final String USER = "root";
-   // private static final String PASS = "root";
-    
-
-    
-    private final String query = "SELECT subject_user.id AS S_U_ID, subject.number AS SUBJECT_N, lang.number AS LANG_N, city_passage.number AS CITY_PASSAGE_N, area.number AS AREA_N, city.number AS CITY_N, region.number AS REGION_N, city_type, users.id AS U_ID, bill\n" +
-                        "FROM `subject_user`\n" +
-                        "JOIN `users` ON users.id = subject_user.user_id\n" +
-                        "JOIN `subject` ON subject_id = subject.id\n" +
-                        "JOIN `lang` ON lang.id = lang_id\n" +
-                        "JOIN `city_passage` ON city_passage.id = city_passage\n" +
-                        "JOIN `area` ON area.id = obl\n" +
-                        "LEFT OUTER JOIN `city` ON city.id = area\n" +
-                        "LEFT OUTER JOIN `region` ON region.id = area\n" +
-                        "WHERE subject_user.end = 1\n" +
-                        "ORDER BY `users`.`id` ASC"; 
-    
     private boolean isInitDone = false;
-    
+
     private Connection con = null;
     private Statement stmt = null;
     private Statement updateStmt = null;
     private ResultSet rs = null;
-    
-    
-    
-    boolean init(){     //Возвращает true если все норм 
+
+    /**
+     * Init Database connection
+     *
+     * @return boolean - is init OK
+     */
+    boolean init() {     //Возвращает true если все норм
         boolean returnStatus = false;
         appSettings currentSettings = new appSettings();
-        
-        try{
-            con = DriverManager.getConnection("jdbc:mysql://"+currentSettings.fields.getdBUrl()+":3306/"
-                    +currentSettings.fields.getdBTableName(),
+
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://" + currentSettings.fields.getdBUrl() + ":3306/"
+                            + currentSettings.fields.getdBTableName(),
                     currentSettings.fields.getdBUser(),
                     currentSettings.fields.getdBPassword());
             stmt = con.createStatement();
             updateStmt = con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE);
-            System.out.println("Database connection initialization OK!"); 
+            System.out.println("Database connection initialization OK!");
             isInitDone = true;
             returnStatus = true;
         } catch (SQLException sqlEx) {
-            //sqlEx.printStackTrace();
-            //Героически молчим, красный текст в окне и так появися, а трэйс не поможет понять ошибку
+            JOptionPane.showMessageDialog(null, sqlEx.getLocalizedMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            sqlEx.printStackTrace();
+
         }
         return returnStatus;
     }
-    
-    int checkAndUpdateBills(String [] problemsToReturn){  //Возвращает количество обновленных строчек
+
+    int checkAndUpdateBills(String[] problemsToReturn) {  //Возвращает количество обновленных строчек
         int rowUpdatedCounter = 0;
         if (!isInitDone) init();
-        try{
+        try {
+            String query = """
+                    SELECT subject_user.id AS S_U_ID, subject.number AS SUBJECT_N, lang.number AS LANG_N, city_passage.number AS CITY_PASSAGE_N, area.number AS AREA_N, city.number AS CITY_N, region.number AS REGION_N, city_type, users.log AS U_LOGIN, bill
+                    FROM `subject_user`
+                    JOIN `users` ON users.id = subject_user.user_id
+                    JOIN `subject` ON subject_id = subject.id
+                    JOIN `lang` ON lang.id = lang_id
+                    JOIN `city_passage` ON city_passage.id = city_passage
+                    JOIN `area` ON area.id = obl
+                    LEFT OUTER JOIN `city` ON city.id = area
+                    LEFT OUTER JOIN `region` ON region.id = area
+                    WHERE subject_user.end = 1
+                    ORDER BY `users`.`id` ASC""";
             rs = stmt.executeQuery(query);
-            
-            String subjectUserId = "";
-            String subjectId = "";
-            String langId = "";
-            String cityPassageId = "";
-            String areaId = "";
-            String cityType = "";
-            String cityId = "";
-            String regionId = "";
-            int userId = 0;
-            String userId4Digit = "";
-            String bill = "";
-            String newBill = "";
-            String status = "";
 
             int i = 0;
             int j = 0;
 
-            while(rs.next()){
+            while (rs.next()) {
                 //----- get IDs from DB
-                subjectUserId = rs.getString("S_U_ID");
-                subjectId = rs.getString("SUBJECT_N");
-                langId = rs.getString("LANG_N");
-                cityPassageId = rs.getString("CITY_PASSAGE_N");
-                areaId = rs.getString("AREA_N");
-                cityType = rs.getString("city_type");
-                cityId = rs.getString("CITY_N");
-                regionId = rs.getString("REGION_N");
-                userId = rs.getInt("U_ID");
-                userId4Digit = Integer.toString(userId + 10000);
-                bill = rs.getString("bill");
+                String subjectUserId = rs.getString("S_U_ID");
+                String subjectId = rs.getString("SUBJECT_N");
+                String langId = rs.getString("LANG_N");
+                String cityPassageId = rs.getString("CITY_PASSAGE_N");
+                String areaId = rs.getString("AREA_N");
+                String cityType = rs.getString("city_type");
+                String cityId = rs.getString("CITY_N");
+                String regionId = rs.getString("REGION_N");
+                String userId4Digit = Integer.toString(rs.getInt("U_LOGIN"));
+                String bill = rs.getString("bill");
 
-                if (bill == null) bill = "";                    //если bill в базе "null", то будет выброшен NullPointerException
-                if (regionId == null) regionId = "";            //если regionId в базе "null" и человек менял место жительства, то это может сделать плохо =) 
-                if (langId == null) langId = "0";               //"null" как поле в запросе это не текст, это null-object
-                if (cityId == null) cityId = "";                //Проблем не было, но пусть будет на всякий случай, зачем лишний null-объект?
+                if (bill == null) {
+                    bill = "";
+                }
+
+                if (regionId == null) {
+                    regionId = "";
+                }
+
+                if (langId == null) {
+                    langId = "0";
+                }
+
+                if (cityId == null) {
+                    cityId = "";
+                }
+
 
                 //----- Формируем новый bill:
-                newBill = "";
-                newBill+=subjectId;
-                newBill+=langId;         
-                newBill+=cityPassageId;
-                newBill+=areaId;
-                newBill+=(cityType.equals("0"))? regionId : cityId;
-                newBill+=userId4Digit;
+                StringBuilder newBill = new StringBuilder();
+                newBill.append(subjectId);
+                newBill.append(langId);
+                newBill.append(cityPassageId);
+                newBill.append(areaId);
+                newBill.append((cityType.equals("0")) ? regionId : cityId);
+                newBill.append(userId4Digit);
                 //-----
 
-                if (bill.equals(newBill)) {
+                String status;
+
+                if (bill.equals(newBill.toString())) {
                     status = "BILL OK";
-                } else if (bill.equals("")){
-                    updateStmt.executeUpdate("UPDATE `subject_user` SET `bill` = \""+newBill+"\" WHERE `subject_user`.`id` = "+subjectUserId);
+                } else if (bill.equals("")) {
+                    updateStmt.executeUpdate("UPDATE `subject_user` SET `bill` = \"" + newBill + "\" WHERE `subject_user`.`id` = " + subjectUserId);
                     status = "ROW UPDATED!";
                     rowUpdatedCounter++;
                 } else {
                     status = "WTF?";
                 }
-                
-                if (status.equals("WTF?")) problemsToReturn[i++] = "Код в базе <"+bill+"> отличается от вычисленного <"+newBill+">!";
-                /*if (status.equals("WTF?"))*/ System.out.printf("%4d. %2s | %s | %2s | %1s | %4s | %2s | %5s | %13s << = >> %13s -%s\n",j++, subjectId, langId, cityPassageId, areaId, cityId, regionId, userId4Digit, bill, newBill, status);
-                if (bill.equals(newBill)) System.out.println("  -GOTCHA!");
-                else {System.out.println("  x WRONG!");}
+
+                if (status.equals("WTF?"))
+                    problemsToReturn[i++] = "Код в базе <" + bill + "> отличается от вычисленного <" + newBill + ">!";
+                /*if (status.equals("WTF?"))*/
+                System.out.printf("%4d. %2s | %s | %2s | %1s | %4s | %2s | %5s | %13s << = >> %13s -%s\n", j++, subjectId, langId, cityPassageId, areaId, cityId, regionId, userId4Digit, bill, newBill, status);
+                if (bill.equals(newBill.toString())) System.out.println("  -GOTCHA!");
+                else {
+                    System.out.println("  x WRONG!");
+                }
             }
         } catch (SQLException sqlEx) {
             sqlEx.printStackTrace();
         } finally {
-            //close connection, stmt and resultset here
-            try {if(con != null) con.close(); } catch(SQLException se) { /*can't do anything */ }
-            try {if(stmt != null) stmt.close(); } catch(SQLException se) { /*can't do anything */ }
-            try {if(updateStmt != null) updateStmt.close(); } catch(SQLException se) { /*can't do anything */ }
-            try {if(rs != null) rs.close(); } catch(SQLException se) { /*can't do anything */ }
+            //close connection, stmt and result set here
+            try {
+                if (con != null) con.close();
+            } catch (SQLException se) { /*can't do anything */ }
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se) { /*can't do anything */ }
+            try {
+                if (updateStmt != null) updateStmt.close();
+            } catch (SQLException se) { /*can't do anything */ }
+            try {
+                if (rs != null) rs.close();
+            } catch (SQLException se) { /*can't do anything */ }
             isInitDone = false; //init Undone
-        }          
+        }
         return rowUpdatedCounter;
     }
-    
-    boolean writePayment(String billToFind) throws NullPointerException{    //Принимает строку из 13ти цифр кода оплаты
+
+    boolean writePayment(String billToFind) throws NullPointerException {    //Принимает строку из 13ти цифр кода оплаты
         boolean writeStatus = false;
-        int paymentId = 0;
-        if (billToFind.matches("\\d{13}")){     //Проверка аргумента перед SQL запросом
-            try{
+        int paymentId;
+        if (billToFind.matches("\\d{13}")) {     //Проверка аргумента перед SQL запросом
+            try {
                 stmt = con.createStatement();
                 updateStmt = con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE);
-                rs = stmt.executeQuery("SELECT `id`, `payment`, COUNT(`id`) AS ROW_NUMBER FROM `subject_user` WHERE `bill` LIKE \""+billToFind+"\"");
+                rs = stmt.executeQuery("SELECT `id`, `payment`, COUNT(`id`) AS ROW_NUMBER FROM `subject_user` WHERE `bill` LIKE \"" + billToFind + "\"");
                 rs.next();
-                if ((rs.getString("ROW_NUMBER").equals("1"))&&(rs.getInt("payment") == 0)){
+                if ((rs.getString("ROW_NUMBER").equals("1")) && (rs.getInt("payment") == 0)) {
                     paymentId = rs.getInt("id");
-                    System.out.println("Payment id = "+paymentId);                                              //!!!
-                    updateStmt.executeUpdate("UPDATE `subject_user` SET payment = 1 WHERE `id` = "+paymentId);
+                    System.out.println("Payment id = " + paymentId);
+                    updateStmt.executeUpdate("UPDATE `subject_user` SET payment = 1 WHERE `id` = " + paymentId);
                     writeStatus = true;
-                    System.out.println("Bill paid");                                                            //!!!
-                } else {
-                    //System.err.println("Bill <"+billToFind+"> not unique or already paid!");
+                    System.out.println("Bill paid");
                 }
-
             } catch (SQLException sqlEx) {
+                JOptionPane.showMessageDialog(null, sqlEx.getLocalizedMessage(), "Error", JOptionPane.ERROR_MESSAGE);
                 sqlEx.printStackTrace();
             }
-            
-                    
-        } else{
+
+        } else {
             //System.err.println("Bill not 13 digits!");
             //Проверим аргумент функции еще раз перед запросом в базу
             JOptionPane.showMessageDialog(null, "Bill not 13 digits!", "Error", JOptionPane.ERROR_MESSAGE);
         }
         return writeStatus;
     }
-    
-    void closeConnection(){
-        try {if(con != null) con.close(); } catch(SQLException se) { /*can't do anything */ }
-        try {if(stmt != null) stmt.close(); } catch(SQLException se) { /*can't do anything */ }
-        try {if(updateStmt != null) updateStmt.close(); } catch(SQLException se) { /*can't do anything */ }
-        try {if(rs != null) rs.close(); } catch(SQLException se) { /*can't do anything */ }
+
+    void closeConnection() {
+        try {
+            if (con != null) con.close();
+        } catch (SQLException se) { /*can't do anything */ }
+        try {
+            if (stmt != null) stmt.close();
+        } catch (SQLException se) { /*can't do anything */ }
+        try {
+            if (updateStmt != null) updateStmt.close();
+        } catch (SQLException se) { /*can't do anything */ }
+        try {
+            if (rs != null) rs.close();
+        } catch (SQLException se) { /*can't do anything */ }
         isInitDone = false; //init Undone      
     }
 }
